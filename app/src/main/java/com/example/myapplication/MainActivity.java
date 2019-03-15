e0ff6381d1db4d41a0f3d0c85662d9727846ab14
juliof9 <00081817@uca.edu.sv>
2019-03-15 12:23:09 -0600

    Terminando

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,28 +14,33 @@ public class MainActivity extends AppCompatActivity {
     private TextView txt1;
     private EditText user;
     private EditText pass;
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        txt1 = (TextView) findViewById(R.id.txt1);
+        user = (EditText) findViewById(R.id.user);
+        pass = (EditText) findViewById(R.id.pass);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(clickListener);
+        btn1.setOnLongClickListener(clickListener2);
     }
 
     private View.OnClickListener clickListener = new View.OnClickListener(){
         @Override
         public void onClick(View view){
             txt1.setText(user.getText());
-        }
+        };
     };
 
-    private View.OnLongClickListener LongClickListener = new View.OnLongClickListener(){
+    private View.OnLongClickListener clickListener2 = new View.OnLongClickListener(){
         @Override
-        public void onClick(View view){
+        public boolean onLongClick(View view){
             txt1.setText(pass.getText());
-        }
+            return true;
+        };
     };
-
 
 }
